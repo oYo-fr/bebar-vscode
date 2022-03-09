36d46daf59ca285b979e98009c2eb22f6dfbad06
yoannd <yoann.diguet@gmail.com>
2022-03-09 03:17:20 +0100

    Externalize web view HTML in a separate file
    (And use bebar to pass some parameters)

--- src/BebarEditor.ts
+++ src/BebarEditor.ts
@@ -1,5 +1,11 @@
 import * as vscode from "vscode";
 
+import * as fs from 'fs';
+import * as util from 'util';
+const readFile = util.promisify(fs.readFile);
+import path = require("path");
+import { Bebar, BebarHandler, Dataset, Template } from "bebar";
+
 export class BebarEditor implements vscode.CustomTextEditorProvider {
 	public static currentPanel: BebarEditor | undefined;
 
@@ -24,7 +30,7 @@ export class BebarEditor implements vscode.CustomTextEditorProvider {
 		webviewPanel.webview.options = {
 			enableScripts: true,
 		};
-		webviewPanel.webview.html = this._getHtmlForWebview(webviewPanel.webview);
+		webviewPanel.webview.html = await this._getHtmlForWebview(webviewPanel.webview);
 
 		function updateWebview() {
 			webviewPanel.webview.postMessage({
@@ -70,55 +76,94 @@ export class BebarEditor implements vscode.CustomTextEditorProvider {
 		updateWebview();
 	}
 
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Local path to script and css for the webview
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'catScratch.js'));
+  /**
+   * Returns the absolute path to a file located in our misc folder.
+   *
+   * @param file The base file name.
+   * @param context The context of this extension to get its path regardless where it is installed.
+   */
+  public static getResourcesPath(file: string, context: vscode.ExtensionContext, asUri = false): string {
+    if (asUri) {
+        return vscode.Uri.file(context.asAbsolutePath(path.join('resources', file))).toString();
+    }
+    return vscode.Uri.file(context.asAbsolutePath(path.join('resources', file))).fsPath;
+  }
 
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'reset.css'));
-
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'vscode.css'));
-
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'catScratch.css'));
-
-		// Use a nonce to whitelist which scripts can be run
+	private async _getHtmlForWebview(webview: vscode.Webview) {
+    // Use a nonce to whitelist which scripts can be run
 		const nonce = getNonce();
+    const htmlFilePath = BebarEditor.getResourcesPath("BebarEditor/index.html", this.context);
 
-		return /* html */`
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
+    const htmlBebarHandler = new BebarHandler(new Bebar({
+      templates: [
+        new Template({
+          file: htmlFilePath
+        })
+      ],
+      data: [
+        new Dataset({
+          name: 'context',
+          content: {
+            nonce: nonce,
+            cspSource: webview.cspSource
+          }
+        })
+      ]
+    }));
 
-				<!--
-				Use a content security policy to only allow loading images from https or from our extension directory,
-				and only allow scripts that have a specific nonce.
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+    try{
+      await htmlBebarHandler.load();
+    }catch (e) {
+      console.log(e);
+    }
 
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    return htmlBebarHandler.templateHandlers[0].outputs[0].content;
 
-				<link href="${styleResetUri}" rel="stylesheet" />
-				<link href="${styleVSCodeUri}" rel="stylesheet" />
-				<link href="${styleMainUri}" rel="stylesheet" />
+    // const html = await readFile(path, "utf8");
+		// // Local path to script and css for the webview
+		// const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'catScratch.js'));
 
-				<title>Cat Scratch</title>
-			</head>
-			<body>
-				<div class="notes">
-					<div class="add-button">
-						<button>Scratch!</button>
-					</div>
-				</div>
+		// const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'reset.css'));
 
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
+		// const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'vscode.css'));
+
+		// const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'catScratch.css'));
+
+		// return /* html */`
+		// 	<!DOCTYPE html>
+		// 	<html lang="en">
+		// 	<head>
+		// 		<meta charset="UTF-8">
+
+		// 		<!--
+		// 		Use a content security policy to only allow loading images from https or from our extension directory,
+		// 		and only allow scripts that have a specific nonce.
+		// 		-->
+		// 		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+		// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		// 		<link href="${styleResetUri}" rel="stylesheet" />
+		// 		<link href="${styleVSCodeUri}" rel="stylesheet" />
+		// 		<link href="${styleMainUri}" rel="stylesheet" />
+
+		// 		<title>Cat Scratch</title>
+		// 	</head>
+		// 	<body>
+		// 		<div class="notes">
+		// 			<div class="add-button">
+		// 				<button>Scratch!</button>
+		// 			</div>
+		// 		</div>
+
+		// 		<script nonce="${nonce}" src="${scriptUri}"></script>
+		// 	</body>
+		// 	</html>`;
 	}
-
 }
 
 function getNonce() {
